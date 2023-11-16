0fa5c201458f4eb8bfe54888b61d299bcc4f06d9
hehoku <hehoku@outlook.com>
2023-11-16 22:32:52 +0800

    chore: Add settings for cSpell, tailwind config, and new libraries. Update HTML and sketch.js files.

--- sketch.js
+++ sketch.js
@@ -1,28 +1,53 @@
 let song, analyzer;
 
 function preload() {
-  song = loadSound("./assets/宋亚轩 - 我怀念的 (Live).mp3");
+  song = loadSound("./assets/SpotifyMate.com - 陀螺 - 万晓利.mp3");
 }
 
+// We define our palette
+let palette = [
+  "#2c695a",
+  "#4ad6af",
+  "#7facc6",
+  "#4e93cc",
+  "#f6684f",
+  "#ffd300",
+];
+
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(800, 600, WEBGL);
   song.loop();
-
-  // create a new Amplitude analyzer
   analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
   analyzer.setInput(song);
+
+  angleMode(DEGREES);
+  background("#fffceb");
+
+  // Scale brushes to adapt to canvas size
+  brush.scale(1.5);
+
+  // Activate the flowfield we're going to use
+  brush.field("seabed");
 }
 
 function draw() {
-  background(255);
-
-  // Get the average (root mean square) amplitude
   let rms = analyzer.getLevel();
-  fill(127);
-  stroke(0);
+  console.log(rms);
+  frameRate(10);
+  translate(-width / 2, -height / 2);
 
-  // Draw an ellipse with size based on volume
-  ellipse(width / 2, height / 2, 10 + rms * 200, 10 + rms * 200);
+  // brush.box() returns an array with available brushes
+  let available_brushes = brush.box();
+
+  // Set the stroke to a random brush, color, and weight = 1
+  // You set a brush like this: brush.set(name_brush, color, weight)
+  brush.set(random(available_brushes), random(palette), 1);
+
+  // Draw a random flowLine (x, y, length, direction)
+  brush.flowLine(
+    random(width),
+    random(height),
+    random(300, 800),
+    random(0, 360)
+  );
 }
